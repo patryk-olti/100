06ab8461ebe32572babf575ba3cc475a20cda074
Patryk Oltuch <patryk.oltuch@datacourage.com>
2024-02-08 18:19:10 +0100

    Created mechanism of getting location.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    })
+  })
+
   return (
     <div className="App">
       Hello world
